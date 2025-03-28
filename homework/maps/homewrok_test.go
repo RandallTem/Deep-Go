3afb80691258e6adc05ce0cf85f36acec8af20a5
Randall <randallzmey72@gmail.com>
2025-03-28 16:09:30 +0300

    Домашнее задание №4

--- homework/maps/homewrok_test.go
+++ homework/maps/homewrok_test.go
@@ -9,32 +9,154 @@ import (
 
 // go test -v homework_test.go
 
+type Node struct {
+	key        int
+	value      int
+	leftChild  *Node
+	rightChild *Node
+}
+
 type OrderedMap struct {
-	// need to implement
+	root *Node
+	size int
 }
 
 func NewOrderedMap() OrderedMap {
-	return OrderedMap{} // need to implement
+	return OrderedMap{}
 }
 
 func (m *OrderedMap) Insert(key, value int) {
-	// need to implement
+	if m.root == nil {
+		m.root = &Node{key, value, nil, nil}
+		m.size++
+		return
+	}
+	parentNode := m.root
+	currentNode := m.root
+	for currentNode != nil {
+		parentNode = currentNode
+		if currentNode.key > key {
+			currentNode = currentNode.leftChild
+		} else {
+			currentNode = currentNode.rightChild
+		}
+	}
+	m.size++
+	if parentNode.key > key {
+		parentNode.leftChild = &Node{key, value, nil, nil}
+	} else {
+		parentNode.rightChild = &Node{key, value, nil, nil}
+	}
 }
 
 func (m *OrderedMap) Erase(key int) {
-	// need to implement
+	var parentNode *Node
+	currentNode := m.root
+	for currentNode != nil && currentNode.key != key {
+		parentNode = currentNode
+		if currentNode.key > key {
+			currentNode = currentNode.leftChild
+		} else {
+			currentNode = currentNode.rightChild
+		}
+	}
+	switch {
+	case currentNode == nil:
+		return
+	case currentNode.leftChild == nil && currentNode.rightChild == nil:
+		if currentNode == parentNode.rightChild {
+			if parentNode != nil {
+				parentNode.rightChild = nil
+			} else {
+				m.root = nil
+			}
+		} else {
+			if parentNode != nil {
+				parentNode.leftChild = nil
+			} else {
+				m.root = nil
+			}
+		}
+	case currentNode.leftChild == nil && currentNode.rightChild != nil:
+		if currentNode == parentNode.rightChild {
+			if parentNode != nil {
+				parentNode.rightChild = currentNode.rightChild
+			} else {
+				m.root = currentNode.rightChild
+			}
+		} else {
+			if parentNode != nil {
+				parentNode.leftChild = currentNode.rightChild
+			} else {
+				m.root = currentNode.rightChild
+			}
+		}
+	case currentNode.leftChild != nil && currentNode.rightChild == nil:
+		if currentNode == parentNode.rightChild {
+			if parentNode != nil {
+				parentNode.rightChild = currentNode.leftChild
+			} else {
+				m.root = currentNode.leftChild
+			}
+		} else {
+			if parentNode != nil {
+				parentNode.leftChild = currentNode.leftChild
+			} else {
+				m.root = currentNode.leftChild
+			}
+		}
+	case currentNode.leftChild != nil && currentNode.rightChild != nil:
+		successor, successorParent := findSuccessor(currentNode)
+		if parentNode != nil {
+			parentNode.rightChild = successor
+		} else {
+			m.root = successor
+		}
+		successor.leftChild = currentNode.leftChild
+		successor.rightChild = currentNode.rightChild
+		if successorParent != currentNode {
+			successorParent.leftChild = successor.rightChild
+		}
+	}
+	m.size--
+}
+
+func findSuccessor(node *Node) (successor *Node, parent *Node) {
+	successor = node.rightChild
+	for successor.leftChild != nil {
+		parent = successor
+		successor = successor.leftChild
+	}
+	return successor, parent
 }
 
 func (m *OrderedMap) Contains(key int) bool {
-	return false // need to implement
+	currentNode := m.root
+	for currentNode != nil && currentNode.key != key {
+		if currentNode.key > key {
+			currentNode = currentNode.leftChild
+		} else {
+			currentNode = currentNode.rightChild
+		}
+	}
+	return currentNode != nil
 }
 
 func (m *OrderedMap) Size() int {
-	return 0 // need to implement
+	return m.size
 }
 
 func (m *OrderedMap) ForEach(action func(int, int)) {
-	// need to implement
+	inOrderBypass(m.root, action)
+}
+
+func inOrderBypass(node *Node, action func(int, int)) {
+	if node == nil {
+		return
+	}
+	inOrderBypass(node.leftChild, action)
+	action(node.key, node.value)
+	inOrderBypass(node.rightChild, action)
 }
 
 func TestCircularQueue(t *testing.T) {
